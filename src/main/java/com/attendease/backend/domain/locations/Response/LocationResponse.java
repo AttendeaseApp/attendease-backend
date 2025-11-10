a33b7c8f5e49aff3c304a1c96aa16b4f1331980f
jakeviado <jakematthewviado204@gmail.com>
2025-11-10 11:50:46 +0800

    location response (#43)

--- src/main/java/com/attendease/backend/domain/locations/Response/LocationResponse.java
+++ src/main/java/com/attendease/backend/domain/locations/Response/LocationResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +15,6 @@ public class LocationResponse {
     private String locationType;
     private Double latitude;
     private Double longitude;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
