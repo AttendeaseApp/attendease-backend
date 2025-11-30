44056c2019fab2fca91beccc469ae6f1705cd8d0
jakeviado <jakematthewviado204@gmail.com>
2025-12-01 01:52:08 +0800

    feat: websocket fixes and controllers (#81)
    * websockets
    
    * re-enable facial service on event registration

--- src/main/java/com/attendease/backend/studentModule/service/event/retrieval/EventRetrievalService.java
+++ src/main/java/com/attendease/backend/studentModule/service/event/retrieval/EventRetrievalService.java
@@ -28,22 +28,6 @@ public class EventRetrievalService {
     private final EventSessionsRepository eventSessionRepository;
 
     /**
-     * Retrieves all events that are either:
-     * <ul>
-     *     <li>ONGOING</li>
-     *     <li>UPCOMING</li>
-     *     <li>REGISTRATION</li>
-     * </ul>
-     *
-     * <p>This is primarily used to display events that students may interact with.</p>
-     *
-     * @return a list of event sessions matching any of the allowed statuses
-     */
-    public List<EventSessions> getOngoingRegistrationAndActiveEvents() {
-        return eventSessionRepository.findByEventStatusIn(Arrays.asList(EventStatus.ONGOING, EventStatus.UPCOMING, EventStatus.REGISTRATION));
-    }
-
-    /**
      * Retrieves event details by its unique identifier.
      *
      * @param id the ID of the event session
