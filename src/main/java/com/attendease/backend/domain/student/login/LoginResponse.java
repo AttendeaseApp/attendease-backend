b9af44e609a2964ff3d5dadfcd8499fced968e9b
jakeeviado <jakematthewviado204@gmail.com>
2025-12-21 00:13:32 +0800

    fix(login): Added JWT token in headers instead of response

--- src/main/java/com/attendease/backend/domain/student/login/LoginResponse.java
+++ src/main/java/com/attendease/backend/domain/student/login/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.attendease.backend.domain.student.login;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +19,10 @@ import lombok.NoArgsConstructor;
 public class LoginResponse {
 
     private boolean success;
+
+    @JsonSerialize
     private String token;
+
     private boolean requiresFacialRegistration;
     private String message;
     private String studentNumber;
--- src/main/java/com/attendease/backend/student/controller/authentication/login/AuthenticationLoginController.java
+++ src/main/java/com/attendease/backend/student/controller/authentication/login/AuthenticationLoginController.java
@@ -5,11 +5,15 @@ import com.attendease.backend.domain.student.login.LoginResponse;
 import com.attendease.backend.student.service.authentication.login.AuthenticationLoginService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.attendease.backend.security.constants.SecurityConstants.JWT_TOKEN_HEADER;
+import static com.attendease.backend.security.constants.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/auth/student")
@@ -23,7 +27,17 @@ public class AuthenticationLoginController {
      */
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> loginStudent(@RequestBody LoginRequest request) {
-        LoginResponse response = authenticationLoginService.loginStudent(request.getStudentNumber(), request.getPassword());
+        LoginResponse response = authenticationLoginService.loginStudent(
+                request.getStudentNumber(),
+                request.getPassword()
+        );
+
+        if (response.isSuccess() && response.getToken() != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(JWT_TOKEN_HEADER, TOKEN_PREFIX + response.getToken());
+            return ResponseEntity.ok().headers(headers).body(response);
+        }
+
         return ResponseEntity.ok(response);
     }
 
