19e24aa4d2320b4fb88c958979fa272416ba9110
jakeeviado <jakematthewviado204@gmail.com>
2025-12-26 02:44:50 +0800

    feat: properties changes on this branch hehe

--- src/main/java/com/attendease/backend/configurations/MongoConfig.java
+++ src/main/java/com/attendease/backend/configurations/MongoConfig.java
@@ -4,17 +4,14 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -22,16 +19,17 @@ import java.util.concurrent.TimeUnit;
 @EnableMongoRepositories(basePackages = "com.attendease.backend.repository")
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    private final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
     @Value("${spring.data.mongodb.database:attendease_db}")
     private String databaseName;
 
-    private final Environment environment;
+    @Value("${spring.data.mongodb.uri:#{null}}")
+    private String mongoUri;
 
-    public MongoConfig(Environment environment) {
-        this.environment = environment;
-    }
+    @Value("${spring.data.mongodb.host:localhost}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port:27017}")
+    private int mongoPort;
 
     @Override
     protected String getDatabaseName() {
@@ -42,19 +40,16 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Bean
     public MongoClient mongoClient() {
         String uri;
-
-        if (isProdProfile()) {
-            uri = dotenv.get("MONGODB_ATLAS_URI");
-            if (uri == null) {
-                throw new IllegalStateException("MONGODB_ATLAS_URI is not set in .env file");
-            }
+        if (mongoUri != null && !mongoUri.isEmpty()) {
+            uri = mongoUri;
+            log.info("Connecting to MongoDB Atlas with URI");
         } else {
-            String host = environment.getProperty("spring.data.mongodb.host");
-            int port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("spring.data.mongodb.port")));
-            uri = String.format("mongodb://%s:%d/%s", host, port, databaseName);
+            uri = String.format("mongodb://%s:%d/%s", mongoHost, mongoPort, databaseName);
+            log.info("Connecting to MongoDB at {}:{}/{}", mongoHost, mongoPort, databaseName);
         }
 
-        log.info("Connecting to MongoDB with URI: {}", uri.replaceAll("://.*@", "://***:***@"));
+        String sanitizedUri = uri.replaceAll("://[^@]*@", "://***:***@");
+        log.info("MongoDB URI: {}", sanitizedUri);
 
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(uri))
@@ -76,9 +71,5 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Bean
     public MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongoClient(), getDatabaseName());
-    }
-
-    private boolean isProdProfile() {
-        return environment.matchesProfiles("prod");
     }
 }
