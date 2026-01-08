6d633d61c55396c0228dfb2cf3fd30e791ec6bce
jakeeviado <jakematthewviado204@gmail.com>
2026-01-09 00:56:36 +0800

    feat: added automatic account deactivation when semster ends

--- src/main/java/com/attendease/backend/domain/student/history/SectionHistory.java
+++ src/main/java/com/attendease/backend/domain/student/history/SectionHistory.java
@@ -1,15 +1,19 @@
 package com.attendease.backend.domain.student.history;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class SectionHistory {
 
 	private String sectionId;
 	private String sectionName;
 	private String academicYearId;
+	private String academicYearName;
+	private String yearLevel;
 	private LocalDate startDate;
 	private LocalDate endDate;
 }
