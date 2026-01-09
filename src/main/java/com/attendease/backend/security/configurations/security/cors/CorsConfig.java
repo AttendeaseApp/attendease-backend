4b75c80574ddf75695e67929fe608054865d5f21
jakeeviado <jakematthewviado204@gmail.com>
2026-01-09 10:16:39 +0800

    fix: cors

--- src/main/java/com/attendease/backend/security/configurations/security/cors/CorsConfig.java
+++ src/main/java/com/attendease/backend/security/configurations/security/cors/CorsConfig.java
@@ -16,6 +16,11 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:5173"
+        ));
         configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
